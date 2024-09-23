dda05a67dbe4f75bc66b2a03bf6ac988833b132a
vinyeee <yeevin@naver.com>
2024-09-23 19:17:35 +0900

    cctv onvif first settings

---
+++ src/main/java/biz/bigtablet/cctvonvifbackend/CctvOnvifBackendApplication.java
@@ -0,0 +1,13 @@
+package biz.bigtablet.cctvonvifbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvOnvifBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctvOnvifBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biz/bigtablet/cctvonvifbackend/CctvOnvifBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.bigtablet.cctvonvifbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctvOnvifBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
